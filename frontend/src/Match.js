c090d2f57a9ad013827abe7d08b960c65d2fbbc7
Ethen <ebot14rocks@gmail.com>
2023-05-21 00:58:31 -0400

    Added label for what event is being scouted

--- frontend/src/Match.js
+++ frontend/src/Match.js
@@ -21,20 +21,22 @@ function Match() {
       <div className='d-flex justify-content-center bg-custom align-items-center vh-100 form-custom'>
         <div className='bg-white p-3 rounded signin-custom'>
           <div className='mb-3'>
+            <p>Now Scouting For Event: {selectEvent}</p>
             <label className='label-selectEvent' htmlFor='select-event'><strong>Select Event: </strong></label>
             <select className='select-event' onChange={handleChange}>
               <option value='test_event'>Test Event</option>
               {events.map((event, index) => {
                 return (
-                  <option value={event.event_code}>{event.name}</option>
+                  <option value={event.event_name}>{event.name}</option>
                 )
               })}
             </select>
+
           </div>
 
           <form>
             <div className='mb-3'>
-            <label htmlFor='team-number'><strong>Team Number:</strong></label>
+              <label htmlFor='team-number'><strong>Team Number:</strong></label>
             </div>
           </form>
 
